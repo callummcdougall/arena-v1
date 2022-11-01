9e38f5fff92cfefd77f59048e38b9f7ff51afa4b
callummcdougall <cal.s.mcdougall@gmail.com>
2022-11-01 10:29:15 +0000

    change utils

--- w0d3/utils.py
+++ w0d3/utils.py
@@ -1,8 +1,10 @@
+# %%
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import torch as t
 from IPython.display import display
 import torchvision
+import numpy as np
 import pandas as pd
 
 def plot_loss_and_accuracy(loss_list, accuracy_list):
@@ -81,3 +83,49 @@ def compare_my_resnet_to_pytorch(myresnet):
     )
     with pd.option_context("display.max_rows", None):  # type: ignore
         display(df)
+
+def print_param_count(*models, display_df=True, use_state_dict=True):
+    """
+    display_df: bool
+        If true, displays styled dataframe
+        if false, returns dataframe
+
+    use_state_dict: bool
+        If true, uses model.state_dict() to construct dataframe
+            This will include buffers, not just params
+        If false, uses model.named_parameters() to construct dataframe
+            This misses out buffers (more useful for GPT)
+    """
+    df_list = []
+    gmap_list = []
+    for i, model in enumerate(models, start=1):
+        iterator = model.state_dict().items() if use_state_dict else model.named_parameters()
+        print(f"Model {i}, total params = {sum([param.numel() for name, param in iterator])}")
+        df = pd.DataFrame([
+            {f"name_{i}": name, f"shape_{i}": tuple(param.shape), f"num_params_{i}": param.numel()}
+            for name, param in iterator
+        ]) if (i == 1) else pd.DataFrame([
+            {f"num_params_{i}": param.numel(), f"shape_{i}": tuple(param.shape), f"name_{i}": name}
+            for name, param in iterator
+        ])
+        df_list.append(df)
+        gmap_list.append(np.log(df[f"num_params_{i}"]))
+    df = df_list[0] if len(df_list) == 1 else pd.concat(df_list, axis=1).fillna(0)
+    for i in range(1, len(models) + 1):
+        df[f"num_params_{i}"] = df[f"num_params_{i}"].astype(int)
+    if len(models) > 1:
+        param_counts = [df[f"num_params_{i}"].values.tolist() for i in range(1, len(models) + 1)]
+        if all([param_counts[0] == param_counts[i] for i in range(1, len(param_counts))]):
+            print("All parameter counts match!")
+        else:
+            print("Parameter counts don't match up exactly.")
+    if display_df:
+        s = df.style
+        for i in range(1, len(models) + 1):
+            s = s.background_gradient(cmap="viridis", subset=[f"num_params_{i}"], gmap=gmap_list[i-1])
+        with pd.option_context("display.max_rows", 1000):
+            display(s)
+    else:
+        return df
+
+# %%
